afb44608e85dc89e072a2410dbe4953b23ee3480
zo7 <zo7.flynn@gmail.com>
2015-11-06 11:51:36 -0800

    Complete FaceRecognizer and model class
    Adds a toy test case for FaceRecognizer in the command-line invocation of the
    file. Will move this to a proper test file later (I'm on a plane currently).

--- src/FaceRecognitionModel.py
+++ src/FaceRecognitionModel.py
@@ -5,10 +5,61 @@ Class for computing and storing the learned face feature model.
 
 '''
 
+import numpy as np
+
 
 class FaceRecognitionModel:
-    def __init__(self):
-        pass
+
+    def __init__(self, k_rank=0):
+        '''
+        Initializer for the face recognition model.
+
+        Args (optional):
+            k_rank (int): How many principal components to keep. A value of 0
+                indicates that it should be full-rank. (Default 0)
+        '''
+
+        self.k_rank = k_rank
+        self.is_fitted = False
+
+
+    def fit(self, faces):
+        '''
+        Fits a data matrix of faces to the model and learns features in the
+        optimal subspace.
+
+        Args:
+            faces (numpy.ndarray): Data matrix of face vectors.
+        '''
+
+        # Perform SVD to get eigenvectors
+        U,S,V = np.linalg.svd(faces, full_matrices=False)
+
+        # Store top-k eigenvectors as components
+        if self.k_rank != 0:
+            self.components = U[0:self.k_rank]
+        else:
+            self.components = U
+
+        self.is_fitted = True
+
+
+    def transform(self, face):
+        '''
+        Transforms a set of faces by projecting it into the optimal subspace
+        learned by PCA. Can be either a single face instance or multiple.
+
+        Args:
+            face (numpy.ndarray): Data matrix of face vectors.
+        Returns:
+            numpy.ndarray, the transformed matrix if the model has been fitted.
+                None otherwise.
+        '''
+
+        if self.is_fitted:
+            return self.components.T.dot( face )
+        else:
+            return None
 
 
 # Command-Line Invocation
--- src/FaceRecognizer.py
+++ src/FaceRecognizer.py
@@ -5,14 +5,125 @@ Class for training face recognition models and performing classification.
 
 '''
 
+import numpy as np
+
+from FaceRecognitionModel import *
+from KNNClassifier import *
+
 
 class FaceRecognizer:
-    def __init__(self):
-        pass
+
+    def __init__(self, k_rank=0, k_neighbors=5):
+        '''
+        Constructor for FaceRecognizer.
+
+        Args (optional):
+            k_rank (int): How many principal components to keep.
+            k_neighbors (int): How many neighbors to compare against in the
+                kNN classifier.
+        '''
+
+        self.face_recognition_model = FaceRecognitionModel(k_rank=0)
+        self.knn_classifier = KNNClassifier(k_neighbors=k_neighbors)
+
+
+    def train(self, instances):
+        '''
+        Trains the recognizer with a set of faces.
+
+        Args:
+            instances (list<tuple<int, numpy.ndarray>>): List of label/face
+                data pairs.
+        '''
+
+        # Stack all of the faces together and learn principal components
+
+        faces = None
+
+        for instance in instances:
+            if faces is None: #TODO: Use proper way for this
+                faces = instance[1]
+            else:
+                faces = np.vstack((faces, instance[1]))
+        faces = faces.T
+
+        self.face_recognition_model.fit(faces)
+
+        # Add each class to the kNN classifier
+
+        for instance in instances:
+            label, face = instance
+            t_face = self.face_recognition_model.transform(face)
+            self.knn_classifier.add_sample(label, t_face)
+
+
+    def classify(self, face):
+        '''
+        Classifies a given face from the trained set.
+
+        Args:
+            face (numpy.ndarray): The face to classify.
+        Returns:
+            int, the class the face best belongs to.
+        '''
+
+        t_face = self.face_recognition_model.transform(face)
+        return self.knn_classifier.classify(t_face)
 
 
 # Command-Line Invocation
 
 if __name__ == '__main__':
-    pass
+    ''' Test FaceRecognizer class with toy data. '''
+
+    # Parameters to test with
+    num_classes = 10
+    num_samples = 50
+    num_features = 1000
+    num_tests = 10
+
+    # Function to generate a random feature vector for each class (with some
+    # bleed between dimensions)
+    def feature_for_label(label):
+        return label+num_classes*np.random.rand(num_features)
+
+    # Generate train data
+
+    instances = list()
+
+    for label in range(0, num_classes):
+        for i in range(0, num_samples):
+            instances.append( (label, feature_for_label(label)) )
+
+    # Train the face recognizer
+
+    print 'Training model...'
+
+    face_recognizer = FaceRecognizer()
+    face_recognizer.train(instances)
+
+    # Test accuracy of classifier
+
+    total_correct = float(0)
+    total_overall = float(0)
+
+    for label in range(0, num_classes):
+
+        correct = float(0)
+
+        for i in range(0, num_tests):
+            features = feature_for_label(label)
+            predicted_label = face_recognizer.classify(features)
+            if predicted_label == label:
+                correct += 1
+
+        total_correct += correct
+        total_overall += num_tests
+
+        print 'Accuracy for class {}: {}'.format(label, correct/num_tests)
+
+    print 'Overall accuracy: {}'.format(total_correct/total_overall)
+
+
+    print 'Done'
 
